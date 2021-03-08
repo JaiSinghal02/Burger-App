33309af1745657bd4b6e5da7fcb746c8d7509807
JaiSinghal02 <joyfuljai02@gmail.com>
2021-03-08 18:24:38 +0530

    Added Order History Page with Dynamic Data Fetch From Database

--- src/compnents/Order/PastOrder/PastOrder.js
+++ src/compnents/Order/PastOrder/PastOrder.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './PastOrder.css';
 
-const pastOrder = ()=>{
+
+
+
+const pastOrder = (props)=>{
+    let elements=[]
+    for(let e in props.order){
+        if(+props.order[e]<5) {elements.push(<li><span>{e}: </span>{+props.order[e]}</li>)}
+    }
     return (
         <div className="Past-Order">
             <div className="Past-Order-Pin"></div>
             <p className="Past-Order-Ing">Ingredients:</p>
                 <ul>
-                    <li>Patty: 2</li>
+                    {/* <li>Patty: 2</li>
                     <li>Cheese: 1</li>
                     <li>Salad: 3</li>
-                    <li>Pickle: 2 </li>
+                    <li>Pickle: 2 </li> */}
+                    {elements}
                 </ul>
             
-            <p className="Past-Order-Price">Price: Rs 50</p>
+            <p className="Past-Order-Price">Price: Rs {props.order["Amount"]}</p>
         </div>
     )
 };
--- src/containers/OrderHistory/OrderHistory.js
+++ src/containers/OrderHistory/OrderHistory.js
@@ -1,18 +1,62 @@
 import React, {Component} from 'react';
 import './OrderHistory.css'
 import PastOrder from '../../compnents/Order/PastOrder/PastOrder';
+import Spinner from '../../compnents/UI/Spinner/Spinner';
+import axios from 'axios';
 
 class OrderHistory extends Component{
     constructor(props){
         super(props);
+        this.state={
+            orders: [],
+            spinner:true
+        }
     }
-    
+    componentDidMount(){
+        axios.get('https://burger-bing-react-app-default-rtdb.firebaseio.com/orders.json')
+            .then(res=>{
+                let obj=[];
+                for(let eachobj in res.data){
+                    obj.push({
+                        ...res.data[eachobj],
+                        id:eachobj
+                    })
+                }
+                this.setState({orders:obj,spinner:false});
+            })
+            .catch(err=>{
+                this.setState({spinner:false});
+                console.log(err);
+            })
+    }
     render(){
+        let Orders=<Spinner/>
+        let Heading,ExtraCard=null;
+        if(!this.state.spinner){
+            Orders=this.state.orders.map(e=>{
+                return (
+                    <PastOrder order={e} key={e.id}/>
+                )
+            })
+            Heading=<p style={{fontSize:"1.5rem",fontWeight:"bolder"}}>The Orders are:</p>
+            if(Orders.length===0){
+                Orders=<p style={{fontSize:"1.5rem"}}>No Orders to show ...</p>
+                Heading=null;
+            }
+            else if(Orders.length%3!==0){
+                let wid=(Orders.length%3)===1?'56%':'26%';
+                console.log(typeof wid)
+                ExtraCard=<p style={{fontSize:"1rem",width:String(wid),display:'flex',justifyContent:'center',alignItems:'center'}}>Make more orders...</p>
+            }
+        }
         return(
+            <>
+            {Heading}
             <div className="Order-History">
-                <PastOrder/>
-                <PastOrder/>
+                {Orders}
+                {ExtraCard}
             </div>
+            </>
         )
     }
 };
